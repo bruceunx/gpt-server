fb22096573cd9ef91a3ad3516f463b51efbc5eff
bruce <cnhxunoo@gmail.com>
2024-04-21 19:06:38 +0800

    remove docs and redocs url for normal product deployment

--- gpt_server/__main__.py
+++ gpt_server/__main__.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers import gemini as gemini_router  # type: ignore
 from .routers import groq as groq_router  # type: ignore
 
-app = FastAPI()
+app = FastAPI(docs_url=None, swagger_ui_init_oauth=None, redoc_url=None)
 
 
 @app.get("/")
