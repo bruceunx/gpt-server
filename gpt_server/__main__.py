0844538c906585e72985d7a32f3cd384614f7ee2
bruce <cnhxunoo@gmail.com>
2024-05-02 22:21:29 +0800

    :rocket: add cohere api

--- gpt_server/__main__.py
+++ gpt_server/__main__.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers import gemini as gemini_router  # type: ignore
 from .routers import groq as groq_router  # type: ignore
 from .routers import openai as openai_router  # type: ignore
+from .routers import cohere as cohere_router  # type: ignore
 
 app = FastAPI(docs_url=None, swagger_ui_init_oauth=None, redoc_url=None)
 
@@ -22,6 +23,7 @@ app.add_middleware(
 
 app.include_router(gemini_router.router, prefix="/gemini", tags=["gemini"])
 app.include_router(groq_router.router, prefix="/groq", tags=["groq"])
+app.include_router(cohere_router.router, prefix="/cohere", tags=["cohere"])
 app.include_router(openai_router.router,
                    prefix="/openai_chat",
                    tags=["openai"])
---
+++ gpt_server/routers/cohere.py
@@ -0,0 +1,66 @@
+import os
+import json
+from typing import Any
+
+from fastapi import APIRouter, Request
+from sse_starlette import EventSourceResponse
+import aiohttp
+
+from gpt_server.schemas.prompt import Prompt
+
+router = APIRouter()
+
+api_key = os.environ['COHERE_KEY']
+
+url = "https://api.cohere.ai/v1/chat"
+
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json",
+}
+
+
+async def get_content(data):
+    async with aiohttp.ClientSession(headers=headers) as sess:
+        async with sess.post(url, json=data) as res:
+            async for chunk in res.content:
+                if chunk:
+                    tmp = json.loads(chunk.decode("utf-8"))
+                    if tmp["is_finished"]:
+                        yield "[DONE]"
+                    else:
+                        yield json.dumps({
+                            "choices": [{
+                                "index": 0,
+                                "delta": {
+                                    "content": tmp["text"]
+                                }
+                            }]
+                        })
+                else:
+                    yield '{"choices":[{"index":0,"delta":{"content":"error from claude"}}]}'
+                    yield "[DONE]"
+                    break
+
+
+@router.post("/chat", status_code=200)
+async def chat_cohere(request: Request, prompt: Prompt):
+
+    data: dict[str, Any] = {"stream": True}
+    chat_history = []
+    chat_history.append({
+        "role": "SYSTEM",
+        "message": prompt.messages[0].content
+    })
+    less_messages = []
+    if len(prompt.messages) > 6:
+        less_messages = prompt.messages[-4:-1]
+    else:
+        less_messages = prompt.messages[1:-1]
+    for msg in less_messages:
+        chat_history.append({"role": msg.role.upper(), "message": msg.content})
+
+    data["chat_history"] = chat_history
+    data["message"] = prompt.messages[-1].content
+
+    return EventSourceResponse(get_content(data))
